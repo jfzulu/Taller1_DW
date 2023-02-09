8c5ee8d24f65c2af2973e077efc4073e52642814
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2023-02-09 12:57:07 -0500

    Chat terminado

--- prueba.js
+++ prueba.js
@@ -5,6 +5,7 @@ let nombre = document.getElementById('nombre');
 let caracteres = /^[a-zA-Z0-9À-ÿ\s]{0,300}$/;
 let mensajeEnviado = document.getElementById('mensajeEnviado'); 
 let bandera = false;
+const listaMensajes = document.querySelector("#chat-messages");
 
 //error.style.color = 'red';
 
@@ -38,12 +39,21 @@ formulario.addEventListener('submit', (event) => {
     event.preventDefault();
     if(bandera)
     {
-        let cadena = [];
-        cadena.push("Nombre: " + nombre.value + "Mensaje: "+ mensaje.value);
-        mensajeEnviado.innerHTML = cadena.join(', ');
+        const message = mensaje.value;
+        if (!message) return;
+
+        const chatMessage = document.createElement("li");
+        chatMessage.classList.add('messages');
+        chatMessage.textContent = nombre.value + ": "+ message;
+        listaMensajes.appendChild(chatMessage);
+
+        listaMensajes.scrollTop = listaMensajes.scrollHeight;
+        mensaje.value = "";
+        nombre.value = "";
     }
     
 });
+
 /*function sendMessage(){
     let errorMessage = [];
     console.log(message);
