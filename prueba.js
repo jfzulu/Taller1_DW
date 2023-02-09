f1968bd14e38cb26ddec10384cd01f760a7b4bda
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2023-02-08 20:59:24 -0500

    Agregar chat y modificar algunas cosas de otras paginas

--- prueba.js
+++ prueba.js
@@ -1,4 +1,13 @@
-var id;
+const formulario = document.getElementById('formulario');
+let mensaje = document.getElementById('mensaje');
+let nombre = document.getElementById('nombre');
+//const inputs = document.querySelectorAll('#formulario input'); //# = id acceder a todos los inputs del formulario
+let caracteres = /^[a-zA-Z0-9À-ÿ\s]{0,300}$/;
+let mensajeEnviado = document.getElementById('mensajeEnviado'); 
+let bandera = false;
+
+//error.style.color = 'red';
+
 function changeBackground(element){   
     element.className = "cambioColor";
     element.style.border="solid";
@@ -7,5 +16,44 @@ function changeBackground(element){
 }
 function backBackground(element){
     element.className = "vueltaColor";
+    element.style.border="solid";
+    element.style.borderColor="aquamarine"; 
     console.log("de vuelta al Color original");
 }
+
+const validarFormulario = (event) =>{
+    //if(caracteres.test(event.target.value))
+    if(event.target.value.length < 300)
+    {
+        bandera = true;
+        document.querySelector('#grupo__mensaje .formulario__input-error').classList.remove('formulario__input-error-activo');
+    }else{
+        document.querySelector('#grupo__mensaje .formulario__input-error').classList.add('formulario__input-error-activo');
+    }
+}
+
+mensaje.addEventListener('keyup', validarFormulario);
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if(bandera)
+    {
+        let cadena = [];
+        cadena.push("Nombre: " + nombre.value + "Mensaje: "+ mensaje.value);
+        mensajeEnviado.innerHTML = cadena.join(', ');
+    }
+    
+});
+/*function sendMessage(){
+    let errorMessage = [];
+    console.log(message);
+    if(message.value === null || message.value === ''){
+        errorMessage.push("El mensaje tiene más de 300 caracteres");
+    }
+    error.innerHTML = errorMessage.join(', ');
+
+    return false;
+}*/
+
+
+    
